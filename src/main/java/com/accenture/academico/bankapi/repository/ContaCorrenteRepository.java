53e6a7de7ef07983a49cb689130d5c430d73eba2
Joao <florenciojoaofelipe@gmail.com>
2021-04-09 02:01:50 -0300

    adicionando annotation que faltou

--- src/main/java/com/accenture/academico/bankapi/repository/ContaCorrenteRepository.java
+++ src/main/java/com/accenture/academico/bankapi/repository/ContaCorrenteRepository.java
@@ -3,10 +3,11 @@ package com.accenture.academico.bankapi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.accenture.academico.bankapi.model.ContaCorrente;
 
-
+@Repository
 public interface ContaCorrenteRepository  extends JpaRepository<ContaCorrente, Long>{
 
 	public ContaCorrente findTop1ByNumero(String numero);
--- src/main/java/com/accenture/academico/bankapi/repository/ExtratoRepository.java
+++ src/main/java/com/accenture/academico/bankapi/repository/ExtratoRepository.java
@@ -3,9 +3,11 @@ package com.accenture.academico.bankapi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.accenture.academico.bankapi.model.Extrato;
 
+@Repository
 public interface ExtratoRepository extends JpaRepository<Extrato, Long> {
 
 	public List<Extrato> findAllByContaId(Long id);
